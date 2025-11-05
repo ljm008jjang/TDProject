0c41766b6c757820a8ea8bf6846f7b8b0da60ba2
ljm008jjang <ljm008jjang@naver.com>
2025-11-05 18:01:35 +0900

    SetUpProject

---
+++ Source/TDProject/TDProject.Build.cs
@@ -0,0 +1,46 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class TDProject : ModuleRules
+{
+	public TDProject(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"NavigationSystem",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"Niagara",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"TDProject",
+			"TDProject/Variant_Strategy",
+			"TDProject/Variant_Strategy/UI",
+			"TDProject/Variant_TwinStick",
+			"TDProject/Variant_TwinStick/AI",
+			"TDProject/Variant_TwinStick/Gameplay",
+			"TDProject/Variant_TwinStick/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
